5a10415693c98828b6b2267036ad3f4ab0a31786
461kumaa <comnom55@gmail.com>
2025-01-13 14:42:33 +0900

    kadai_020をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const changeText = document.getElementById('text');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  changeText.textContent = 'ボタンをクリックしました';
+});
